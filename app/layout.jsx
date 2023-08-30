88aaa78991571f3d9f7d794947653a44283da56c
mjcoding0702 <chungmangjie2000@gmail.com>
2023-08-30 14:40:27 +0800

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia-V2',
+  description: 'Discover & Share AI Prompts',
+}
+ 
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          {children}
+        </main>
+      
+      </body>
+    </html>
+  )
+}
