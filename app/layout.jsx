2836ace8a51f36abc4366e6aeec12c18e5a9c760
mjcoding0702 <chungmangjie2000@gmail.com>
2023-08-30 16:25:19 +0800

    Added Nav and User Authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,27 @@
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 import '@styles/globals.css';
 
 export const metadata = {
-  title: 'Promptopia-V2',
-  description: 'Discover & Share AI Prompts',
+    title: 'Promptopia',
+    description: 'Discover & Share Cool AI Prompts'
 }
- 
-export default function RootLayout({ children }) {
- return (
-    <html lang="en">
-      <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
 
-        <main className='app'>
-          {children}
-        </main>
-      
-      </body>
+export default function RootLayout({children}){
+  return (
+    <html lang="en">
+        <body>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>
+
+                <main className='app'>
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
+        </body>
     </html>
   )
 }
